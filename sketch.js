ec0956a000b0dff5e85d1b12635ac7d9ea5e807c
koenasaha <98351984+koenasaha@users.noreply.github.com>
2022-02-01 15:38:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var trex ,trex_running;
+function preload(){
+trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex=createSprite(50,160,20,20);
+ trex.addAnimation("running",trex_running);
+ trex.scale=0.7;
+ trex.x=50;
+ground=createSprite(300,190,600,20);
+}
+
+function draw(){
+  background("white")
+  
+if (keyDown("space")){
+  trex.velocityY=-10;
+}
+
+trex.velocityY=trex.velocityY+0.5;
+trex.collide(ground)
+
+drawSprites();
+}
